d87a525d000a12fcf0a637fc306a5064a9ff3b1a
fulgorloge <santiago0160colombia@gmail.com>
2026-08-12 23:57:24 -0500

    Refactor server setup and room management

--- server.js
+++ server.js
@@ -1,53 +1,36 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
-});
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, { cors: { origin: "*" } });
 
-const activeUsers = new Map();
-const activeRooms = new Map();
+let rooms = {}; // Estructura: { roomId: { name, members, playlist: [], currentTrack: null } }
 
 io.on('connection', (socket) => {
-    console.log(`Usuario conectado: ${socket.id}`);
-
     socket.on('update_location', (data) => {
-        activeUsers.set(socket.id, { ...data, id: socket.id });
-        io.emit('rooms_list', Array.from(activeRooms.values()));
+        socket.userData = data;
+        socket.join('global');
     });
 
     socket.on('create_room', (data) => {
-        const roomId = 'room_' + Date.now();
-        const room = { id: roomId, ...data, members: [socket.id] };
-        activeRooms.set(roomId, room);
+        const roomId = Math.random().toString(36).substring(7);
+        rooms[roomId] = { name: data.name, members: [socket.userData.username], playlist: [], currentTrack: null };
         socket.join(roomId);
-        socket.emit('room_created', room);
-        io.emit('rooms_list', Array.from(activeRooms.values()));
+        socket.emit('room_joined', { id: roomId, ...rooms[roomId] });
     });
 
-    socket.on('join_room', (roomId) => {
-        socket.join(roomId);
-        if(activeRooms.has(roomId)) {
-            activeRooms.get(roomId).members.push(socket.id);
-            socket.emit('room_joined', activeRooms.get(roomId));
+    socket.on('add_song', (data) => {
+        if (!rooms[data.roomId]) return;
+        rooms[data.roomId].playlist.push(data.uri);
+        if (!rooms[data.roomId].currentTrack) {
+            rooms[data.roomId].currentTrack = data.uri;
+            io.to(data.roomId).emit('play_now', data.uri);
         }
+        io.to(data.roomId).emit('playlist_updated', rooms[data.roomId].playlist);
     });
 
-    socket.on('chat_msg', ({ roomId, msg, user }) => {
-        io.to(roomId).emit('new_msg', { user, msg, time: new Date().toLocaleTimeString() });
-    });
-
-    socket.on('disconnect', () => {
-        activeUsers.delete(socket.id);
+    socket.on('chat_msg', (data) => {
+        io.to(data.roomId).emit('new_msg', { user: data.user, msg: data.msg, time: new Date().toLocaleTimeString() });
     });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+http.listen(process.env.PORT || 3000);
