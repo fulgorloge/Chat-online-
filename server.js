22532cff132bf386f610b4f71ed4e1281a7d74ac
fulgorloge <santiago0160colombia@gmail.com>
2026-08-15 16:27:34 -0500

    Refactor room management and connection logging

--- server.js
+++ server.js
@@ -9,46 +9,29 @@ const io = new Server(server);
 app.use(express.static('public'));
 app.use(express.json());
 
-let activeRooms = {}; // Registro de salas activas
+let activeRooms = {}; // Almacena salas activas
 
 io.on('connection', (socket) => {
-    // Enviar lista al entrar
     socket.emit('update_room_list', activeRooms);
 
     socket.on('create_room', (data) => {
         socket.join(data.roomName);
-        activeRooms[data.roomName] = { 
-            platform: data.platform, 
-            mediaInput: data.mediaInput,
-            creator: data.user 
-        };
+        activeRooms[data.roomName] = { platform: data.platform, mediaInput: data.mediaInput, creator: data.user };
         io.emit('update_room_list', activeRooms);
-        socket.emit('room_joined', data);
+        socket.emit('room_joined', { roomName: data.roomName, ...activeRooms[data.roomName] });
     });
 
     socket.on('join_room', (data) => {
         socket.join(data.roomName);
-        const roomData = activeRooms[data.roomName];
-        socket.emit('room_joined', { 
-            roomName: data.roomName, 
-            platform: roomData.platform, 
-            mediaInput: roomData.mediaInput 
-        });
-    });
-
-    // Eventos de chat y tipado (mantiene compatibilidad)
-    socket.on('typing', (data) => {
-        const dest = data.scope === 'room' ? data.roomName : 'global';
-        socket.to(dest).emit('display_typing', { user: data.user, scope: data.scope });
+        socket.emit('room_joined', { roomName: data.roomName, ...activeRooms[data.roomName] });
     });
 
     socket.on('room_message', (data) => {
         io.to(data.roomName).emit('room_message', data);
     });
 
-    socket.on('disconnect', () => {
-        console.log(`> Desconectado: ${socket.id}`);
-    });
+    socket.on('disconnect', () => console.log('Nodo desconectado'));
 });
 
-server.listen(3000, () => console.log('> Servidor GeoVibe activo en :3000'));
+const PORT = 3000;
+server.listen(PORT, () => console.log(`> GeoVibe activo en puerto ${PORT}`));
